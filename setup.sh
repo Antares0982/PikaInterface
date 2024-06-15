75d8fcf066edcb2a9277eb8c537269f53d0b326b
antares <antares@alyr.dev>
2024-06-16 02:34:18 +0800

    feat: update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-echo $1
 if [[ $1 == "block" ]]; then
-    curl https://api.github.com/repos/Antares0982/PikaInterface/contents/pika_interface_blocking.py | jq -r ".content" | base64 --decode > antares_pika_interface.py
+    curl -O https://raw.githubusercontent.com/Antares0982/PikaInterface/main/pika_interface_blocking.py
 else
-    curl https://api.github.com/repos/Antares0982/PikaInterface/contents/pika_interface.py | jq -r ".content" | base64 --decode > antares_pika_interface.py
+    curl -O https://raw.githubusercontent.com/Antares0982/PikaInterface/main/pika_interface.py
 fi
